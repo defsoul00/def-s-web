fe573fa06fc16522879a9b34ef5137972d61bbb5
defsoul00 <114261815+defsoul00@users.noreply.github.com>
2024-05-21 14:00:27 +0300

    def's e-commerce site
    ---

--- def's e-commerce site/script.js
+++ def's e-commerce site/script.js
@@ -7,3 +7,11 @@ $(document).ready(function() {
         } 
     });  
   });
+
+  let menu = document.querySelector('#menu-icon');
+  let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
